8f0ff45a8cce1d2092506378d2edc389b8afea0c
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-04 00:30:15 +0530

    Self-learning AI that improves from conversations

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ import re
 from collections import defaultdict
 
 class SelfLearningAI:
+    def __init__(self, name):
+        self.name = name
+        self.knowledge_base = defaultdict(list)
+        self.patterns = []
+        self.context_memory = []
+        self.word_associations = defaultdict(set)
+        self.response_scores = defaultdict(int)
+        self.conversation_count = 0
+        self._initialize_basic_knowledge()
+        
